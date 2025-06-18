9f9073b5162db22cfdace4c238922eec4eef0c0a
dirkstrauss <dirkstrauss@outlook.com>
2025-06-18 07:01:47 +0200

    Update dependencies and improve caching in application
    - Updated `.gitignore` to exclude Visual Studio project evaluation files.
    - Upgraded `Aspire.Hosting.AppHost` to version `9.3.1` and added `Aspire.Hosting.Redis` package.
    - Initialized Redis cache in `Program.cs` to enhance performance and modified method chaining to ensure proper readiness of services.

--- sourcecode/HelloAspire/HelloAspire.AppHost/Program.cs
+++ sourcecode/HelloAspire/HelloAspire.AppHost/Program.cs
@@ -1,10 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.HelloAspire_ApiService>("apiservice");
 
 builder.AddProject<Projects.HelloAspire_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService)
-    .WaitFor(apiService);
+    .WithReference(cache).WaitFor(cache)
+    .WithReference(apiService).WaitFor(apiService);
 
 builder.Build().Run();
